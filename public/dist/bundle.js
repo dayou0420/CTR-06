a378a65353457fc861cd238d04142834d85c2f80
masa <dayoujianghong@gmail.com>
2022-06-25 00:44:05 +0900

    edit src/app.ts

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -52,8 +52,8 @@ Person = __decorate([
     Logger('Output log'),
     withTemplate('<h1>Person Object</h1>', 'app')
 ], Person);
-// const pers = new Person();
-// console.log(pers);
+const pers = new Person();
+console.log(pers);
 function Log(target, propertyName) {
     console.log('Property Decorator');
     console.log(target, propertyName);
@@ -103,6 +103,32 @@ __decorate([
     Log3,
     __param(0, Log4)
 ], Product.prototype, "getPriceWithTax", null);
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+class Printer {
+    constructor() {
+        this.message = 'Clicked';
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+button.addEventListener('click', p.showMessage);
 
 
 /***/ })
--- src/app.ts
+++ src/app.ts
@@ -40,9 +40,9 @@ class Person {
     }
 }
 
-// const pers = new Person();
+const pers = new Person();
 
-// console.log(pers);
+console.log(pers);
 
 function Log(target: any, propertyName: string | Symbol) {
     console.log('Property Decorator');
@@ -98,3 +98,31 @@ class Product {
         return this._price * (1 + tax);
     }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'Clicked';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', p.showMessage);
