19c94229a97fdd18c7a9afa51fb313c100036cba
masa <dayoujianghong@gmail.com>
2022-06-24 20:43:22 +0900

    edit angular-tour-of-heroes

--- server/angular-tour-of-heroes/src/app/app.component.ts
+++ server/angular-tour-of-heroes/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  // title = 'angular-tour-of-heroes';
 }
