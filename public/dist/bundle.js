42dc902e05b3df40b22a1579f1303fd592f320d8
masa <dayoujianghong@gmail.com>
2022-06-27 08:19:29 +0900

    edit src/app.ts

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -11,7 +11,19 @@ class ProjectInput {
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.tempmateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleElement = this.element.querySelector('#title');
+        this.descriptionElement = this.element.querySelector('#description');
+        this.mandayElement = this.element.querySelector('#manday');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     tempmateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleElement: HTMLInputElement;
+    descriptionElement: HTMLInputElement;
+    mandayElement: HTMLInputElement;
 
     constructor() {
         this.tempmateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +12,23 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.tempmateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.mandayElement = this.element.querySelector('#manday') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
