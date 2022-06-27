efbf7d152a03b5e9333b365130ada17713a0afe0
masa <dayoujianghong@gmail.com>
2022-06-28 08:45:28 +0900

    edit src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,61 @@
 /**
- * autobind decorato
+ * Validation
  */
 
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+
+    if (
+        validatableInput.minLength != null && 
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = 
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+
+    if (
+        validatableInput.maxLength != null && 
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = 
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+
+    if (
+        validatableInput.min != null && 
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = 
+            isValid && validatableInput.value >= validatableInput.min;
+    }
+
+    if (
+        validatableInput.max != null && 
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = 
+            isValid && validatableInput.value <= validatableInput.max;
+    }
+
+    return isValid;
+}
+
+/**
+ * autobind decorator
+ */
 function autobind(target: any, name: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
@@ -45,7 +99,29 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredManday = this.mandayInputElement.value;
 
-        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredManday.trim().length === 0) {
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+
+        const mandayValidatable: Validatable = {
+            value: +enteredManday,
+            required: true,
+            min: 1,
+            max: 1000
+        };
+
+        if (
+            !validate(titleValidatable) || 
+            !validate(descriptionValidatable) || 
+            !validate(mandayValidatable)
+        ) {
             alert('Incorrect inputs');
             return;
         } else {
