71c5b8813a32964b6a3eb3c7c433173bb6a04761
masa <dayoujianghong@gmail.com>
2022-07-05 21:40:39 +0900

    add express

---
+++ express/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: {
+        bundle: "./src/app.ts"
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "[name].js"
+    },
+    mode: "development",
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    }
+}
