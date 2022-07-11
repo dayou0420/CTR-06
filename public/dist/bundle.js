ea179e2acce7375e2f9158490b5915204abc06fb
masa <dayoujianghong@gmail.com>
2022-07-12 08:04:08 +0900

    edit src/app.ts

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -5,13 +5,20 @@ var __webpack_exports__ = {};
   !*** ./src/app.ts ***!
   \********************/
 
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 const number1 = 5;
 const number2 = 2.8;
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const resultPrase = 'Result: ';
+add(number1, number2, printResult, resultPrase);
 
 /******/ })()
 ;
