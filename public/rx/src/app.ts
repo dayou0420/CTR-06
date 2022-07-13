cf8018da5b505180527cc71c2a93150c7656dd98
masa <dayoujianghong@gmail.com>
2022-07-13 22:04:54 +0900

    edit src/app.ts

--- public/rx/src/app.ts
+++ public/rx/src/app.ts
@@ -1,37 +1,53 @@
 import { Observable } from 'rxjs';
+import { fromEvent } from 'rxjs';
 
-const observable = new Observable<string>((observer: any) => {
-    try {
-            observer.next('Hey guys!');
-            observer.next('How are you?');
+// const observable = new Observable<string>((observer: any) => {
+//     try {
+//             observer.next('Hey guys!');
+//             observer.next('How are you?');
 
-            setInterval(() => {
-                observer.next('I am good');
-            }, 2000);
+//             setInterval(() => {
+//                 observer.next('I am good');
+//             }, 2000);
 
-            // observer.complete();
-            // observer.next('This will not send');
+//             // observer.complete();
+//             // observer.next('This will not send');
 
-        } catch (err) {
-            observer.error(err);
-    }
-});
+//         } catch (err) {
+//             observer.error(err);
+//     }
+// });
 
-const observer = observable.subscribe({
-    next: (x: any) => addItem(x),
-    error: (error: any) => addItem(error),
-    complete: () => addItem('Completed')
-});
+// const observer = observable.subscribe({
+//     next: (x: any) => addItem(x),
+//     error: (error: any) => addItem(error),
+//     complete: () => addItem('Completed')
+// });
 
-const observer2 = observable.subscribe({
-    next: (x: any) => addItem(x),
-});
+// setTimeout(() => {
+//     observer.unsubscribe();
+//     const observer2 = observable.subscribe({
+//         next: (x: any) => addItem(`Subscribe2 ${x}`)
+//     })
+// }, 1000);
 
-observer.add(observer2);
+// const observer2 = observable.subscribe({
+//     next: (x: any) => addItem(x),
+// });
+
+// observer.add(observer2);
+
+// setTimeout(() => {
+//     observer.unsubscribe();
+// }, 6001);
+
+const observable = fromEvent(document, 'mousemove');
 
 setTimeout(() => {
-    observer.unsubscribe();
-}, 6001)
+    const subscription = observable.subscribe({
+        next: (x: any) => addItem(x)
+    });
+}, 2000);
 
 function addItem(val: string) {
     const node = document.createElement('li');
