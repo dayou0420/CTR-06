1d3dbd2e237541e4b285e31a06f51eaaaa2ef47a
masa <dayoujianghong@gmail.com>
2022-07-20 07:37:06 +0900

    edit src/app.ts

--- public/rx/src/app.ts
+++ public/rx/src/app.ts
@@ -11,11 +11,23 @@ const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
     subscriber.next('Alice');
     subscriber.next('Ben');
-    setTimeout(() => subscriber.next('Charlie'), 2000);
+    setTimeout(() => {
+        subscriber.next('Charlie');
+    }, 2000);
+
+    setTimeout(() => subscriber.error(new Error('Failure')), 4000);
+
+    return () => {
+        console.log('Teardown');
+    };
 });
 
 console.log('Before subscribe');
-observable$.subscribe(value => console.log(value));
+observable$.subscribe({
+    next: value => console.log(value),
+    error: err => console.log(err.message),
+    complete: () => console.log('Completed')
+});
 console.log('After subscribe');
 
 // const observable$ = new Observable<string> (subscriber => {
